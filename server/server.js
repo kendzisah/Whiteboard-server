d8af4370c7a038f0a9bc660a3e00253fddf563e6
kendzisah <dzisahken10@gmail.com>
2024-08-19 22:10:23 -0700

    Make sure all messages are JSON strings

--- server/server.js
+++ server/server.js
@@ -4,12 +4,16 @@ const wss = new WebSocket.Server({ port: 8080 });
 
 wss.on("connection", (ws) => {
   ws.on("message", (message) => {
+    // Ensure the message is a string before broadcasting
+    const messageString = message.toString();
+
+    // Forward the message to all connected clients except the sender
     wss.clients.forEach((client) => {
       if (client !== ws && client.readyState === WebSocket.OPEN) {
-        client.send(message);
+        client.send(messageString);
       }
     });
   });
 });
 
-console.log("Server started on port 8080");
+console.log("WebSocket server started on port 8080");
